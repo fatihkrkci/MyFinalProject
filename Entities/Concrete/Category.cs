fe52796a18cb4dab0efa7047e9c99b89ea87c32b
Fatih Kürekçi <fatihkr02@gmail.com>
2021-02-10 17:59:42 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+    {
+        //Çıplak Class Kalmasın - ÇCK
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
