50e960c42098e8f4ade77440266820dd40d5d470
Fatih Kürekçi <fatihkr02@gmail.com>
2021-02-10 18:17:57 +0300

    10.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //DTO - Data Transformation Object
-            ProductTest();
+            //ProductTest();
             //IoC
             //CategoryTest();
         }
