b4c0237c97dc434f3f63aa65c1fe2f57dbd7a731
Fatih Kürekçi <fatihkr02@gmail.com>
2021-02-25 00:09:14 +0300

    24.02.2021 | Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir Kategoride En Fazla 10 Ürün Olabilir!";
+        public static string ProductNameAlreadyExists = "Bu İsimde Zaten Başka Bir Ürün Var!";
+        public static string CategoryLimitExceded = "Kategori Limiti Aşıldığı İçin Yeni Ürün Eklenemez!";
     }
 }
